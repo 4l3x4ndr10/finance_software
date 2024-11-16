10a24f076844f85552d8a28b6f409fe4cd664f51
4l3x4ndr10 <entakibora66@gmail.com>
2024-11-16 13:09:46 +0300

    added entities

---
+++ src/main/java/entities/Transaction.java
@@ -0,0 +1,41 @@
+package entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transactionID")
+    private Long transactionID;
+
+    @ManyToOne
+    @JoinColumn(name = "userID", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryID")
+    private Category category;
+
+    @Column(name = "amount", nullable = false, precision = 12, scale = 2)
+    private Double amount;
+
+    @Column(name = "type", nullable = false, length = 10)
+    private String type;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+}
+
